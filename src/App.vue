2f559d38fb4160bd0a24a160b91096d938f37e29
pilotvitalik <itvkip@yandex.ru>
2020-02-25 07:06:28 +0300

    Created new common component. Custom fonts activated

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,38 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Wrapper/>
   </div>
 </template>
 
+<script>
+import Wrapper from '@/components/Wrapper.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Wrapper,
+  },
+};
+</script>
+
 <style lang="less">
+@font-face {
+  font-family: 'Poppins-Regular';
+  src: url('./assets/fonts/poppins/Poppins-Regular.eot');
+  src: url('./assets/fonts/poppins/Poppins-Regular.eot?#iefix') format('embedded-opentype'),
+       url('./assets/fonts/poppins/Poppins-Regular.svg#Poppins-Regular') format('svg'),
+       url('./assets/fonts/poppins/Poppins-Regular.ttf') format('truetype'),
+       url('./assets/fonts/poppins/Poppins-Regular.woff') format('woff'),
+       url('./assets/fonts/poppins/Poppins-Regular.woff2') format('woff2');
+  font-weight: normal;
+  font-style: normal;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Poppins-Regular', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/components/Wrapper.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class='wrapper'>
+    <p>This is wrapper</p>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang='less' module>
+</style>
